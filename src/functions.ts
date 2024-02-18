9e45bffbdc41a16aea83fc553f50f82f1bc2e157
MelloFlare <dhirpatel91@gmail.com>
2024-02-18 17:18:02 -0500

    third funciton

--- src/functions.ts
+++ src/functions.ts
@@ -31,7 +31,9 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    const name = message.toUpperCase();
+    const s = "${name}!";
+    return s;
 }
 
 /**
