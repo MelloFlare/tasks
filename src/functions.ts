49e645270262b08ce913cabd7da17512d77aa801
MelloFlare <dhirpatel91@gmail.com>
2024-02-19 20:03:04 -0500

    All funtion test pass

--- src/functions.ts
+++ src/functions.ts
@@ -32,7 +32,7 @@ export function add3(first: number, second: number, third: number): number {
  */
 export function shout(message: string): string {
     const name = message.toUpperCase();
-    const s = "${name}!";
+    const s = name + "!";
     return s;
 }
 
@@ -41,7 +41,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.endsWith("?");
 }
 
 /**
@@ -50,5 +50,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word === "yes" || word === "YES") {
+        return true;
+    } else if (word === "no" || word === "NO") {
+        return false;
+    } else {
+        return null;
+    }
 }
